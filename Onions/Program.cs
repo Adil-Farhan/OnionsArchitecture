7f9c7542e0dfd1d48f329d0226711978a4c8cd5b
AdilFarhan <adilfarhan28@gmail.com>
2025-09-30 15:32:42 +0500

    Onion Architecture implemented with SOLID coding Pinciples. It is an on going project for learning and implementation

---
+++ Onions/Program.cs
@@ -0,0 +1,43 @@
+
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
+using Onions.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+LogManager.Setup().LoadConfigurationFromFile(Path.Combine(Directory.GetCurrentDirectory(), "nlog.config"));
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
+builder.Services.AddControllers().AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly);
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Presentation/Controllers/CompaniesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Service.Contract.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [Route("api/companies")]
+    [ApiController]
+    public class CompaniesController: ControllerBase
+    {
+        private readonly IServiceManager _service;
+
+        public  CompaniesController(IServiceManager service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IActionResult GetCompanies()
+        {
+            try
+            {
+                var companies = _service.CompanyService.GetAllCompanies(false);
+
+                return Ok(companies);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
